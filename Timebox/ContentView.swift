ab1c749a7151d880c5d661ddb7eef291c47e1eae
adrianxx321 <lianghan321@gmail.com>
2022-02-24 05:01:57 +0800

    Create Xcode project

---
+++ Timebox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timebox
+//
+//  Created by Lianghan Siew on 23/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .foregroundColor(.uiGreen)
+            .fontWeight(.heavy)
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Timebox/TimeboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeboxApp.swift
+//  Timebox
+//
+//  Created by Lianghan Siew on 23/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimeboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
