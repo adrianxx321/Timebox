565f79c23d1755c76b4a854dd9336890e6dc8bfd
adrianxx321 <lianghan321@gmail.com>
2022-02-24 05:04:35 +0800

    Export colors from Figma

---
+++ Timebox/View/Extension/Color+extension.swift
@@ -0,0 +1,32 @@
+//  swiftlint:disable all
+//
+//  The code generated using FigmaExport — Command line utility to export
+//  colors, typography, icons and images from Figma to Xcode project.
+//
+//  https://github.com/RedMadRobot/figma-export
+//
+//  Don’t edit this code manually to avoid runtime crashes
+//
+
+import SwiftUI
+
+public extension Color {
+    static var accent: Color { Color(#function) }
+    static var backgroundPrimary: Color { Color(#function) }
+    static var backgroundQuarternary: Color { Color(#function) }
+    static var backgroundSecondary: Color { Color(#function) }
+    static var backgroundTertiary: Color { Color(#function) }
+    static var textPrimary: Color { Color(#function) }
+    static var textSecondary: Color { Color(#function) }
+    static var textTertiary: Color { Color(#function) }
+    static var uiBlack: Color { Color(#function) }
+    static var uiGreen: Color { Color(#function) }
+    static var uiLavender: Color { Color(#function) }
+    static var uiLightPurple: Color { Color(#function) }
+    static var uiMagenta: Color { Color(#function) }
+    static var uiOrange: Color { Color(#function) }
+    static var uiPink: Color { Color(#function) }
+    static var uiPurple: Color { Color(#function) }
+    static var uiRed: Color { Color(#function) }
+    static var uiWhite: Color { Color(#function) }
+}
