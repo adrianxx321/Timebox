0c34a29806859fbef10db5da9942f505a527813c
adrianxx321 <lianghan321@gmail.com>
2022-02-25 19:47:03 +0800

    Discard using NavigationView for navigating between views

--- Timebox/View/LoginScreen.swift
+++ Timebox/View/LoginScreen.swift
@@ -12,54 +12,37 @@ struct LoginScreen: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var body: some View {
-        NavigationView {
-            VStack(spacing: 32) {
-                OnboardingCard(carousel: loginBanner)
-                VStack(spacing: 24) {
-                    Button {} label: {
-                        HStack(alignment: .top, spacing: 8) {
-                            Image(systemName: "applelogo")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 18, height: 18)
-                            Text("Continue with Apple")
-                                .font(.paragraphP2())
-                                .bold()
-                        }
-                        .foregroundColor(.backgroundPrimary)
-                        .padding(.vertical, 12)
-                        .padding(.horizontal, 24)
-                        .background(Capsule()
-                                    .foregroundColor(.uiBlack))
-                    }
-                    Button {} label: {
-                        Text("Other Sign Up Options")
+        VStack(spacing: 32) {
+            
+            OnboardingCard(carousel: loginBanner)
+            VStack(spacing: 24) {
+                Button {} label: {
+                    HStack(alignment: .top, spacing: 8) {
+                        Image(systemName: "applelogo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 18, height: 18)
+                        Text("Continue with Apple")
                             .font(.paragraphP2())
-                            .fontWeight(.medium)
-                            .foregroundColor(.textPrimary)
+                            .bold()
                     }
+                    .foregroundColor(.backgroundPrimary)
+                    .padding(.vertical, 12)
+                    .padding(.horizontal, 24)
+                    .background(Capsule()
+                                .foregroundColor(.uiBlack))
                 }
-                .frame(maxWidth: .infinity)
-                Spacer()
-                Spacer()
-            }
-            .background(Color.backgroundPrimary)
-        }
-        // Hide the default navigation back button...
-        .navigationBarBackButtonHidden(true)
-        // Then create the custom back button...
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button { presentationMode.wrappedValue.dismiss() } label: {
-                    Image("chevron-left")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 32, height: 32)
+                Button {} label: {
+                    Text("Other Sign Up Options")
+                        .font(.paragraphP2())
+                        .fontWeight(.medium)
+                        .foregroundColor(.textPrimary)
                 }
-                .frame(maxWidth: .infinity)
-                .padding(.top, 24)
             }
+            .frame(maxWidth: .infinity)
+            Spacer()
         }
+        .background(Color.backgroundPrimary)
     }
 }
 
--- Timebox/View/OnboardingScreen.swift
+++ Timebox/View/OnboardingScreen.swift
@@ -9,43 +9,55 @@ import SwiftUI
 
 struct OnboardingScreen: View {
     // Current pagination level of carousel, default to 0...
-    @State private var index = 0
+    @State private var offset = 0
+    @State var goToLogin = false
 
     var body: some View {
-        NavigationView {
+        VStack(spacing: 32) {
+            Spacer()
             VStack(spacing: 32) {
-                VStack(spacing: 32) {
-                    TabView(selection: $index) {
-                        ForEach((0..<carousels.count), id: \.self) { index in
-                            OnboardingCard(carousel: carousels[index])
-                        }
+                TabView(selection: $offset) {
+                    ForEach((0..<carousels.count), id: \.self) { index in
+                        OnboardingCard(carousel: carousels[index])
                     }
-                    .tabViewStyle(.page(indexDisplayMode: .never))
-                    HStack(spacing: 4) {
-                        ForEach((0..<3), id: \.self) { index in
-                            Circle()
-                                .fill(index == self.index ? Color.accent : Color.textTertiary)
-                                .frame(width: 10, height: 10)
+                }
+                .tabViewStyle(.page(indexDisplayMode: .never))
+                HStack(spacing: 4) {
+                    ForEach((0..<3), id: \.self) { offset in
+                        Circle()
+                            .fill(offset == self.offset ? Color.accent : Color.textTertiary)
+                            .frame(width: 10, height: 10)
 
-                        }
                     }
                 }
-                NavigationLink(destination: LoginScreen()) {
-                    Text("Get Started")
-                        .font(.subheading1())
-                        .bold()
-                        .foregroundColor(.backgroundPrimary)
-                        .padding(.vertical, 20)
-                        .padding(.horizontal, 40)
-                        .background(Capsule()
-                                        .foregroundColor(.accent)
-                                        .shadow(radius: 12, x: 0, y: 3))
-                }
-                Spacer()
-                Spacer()
             }
-            .background(Color.backgroundPrimary)
+            Button {
+                withAnimation {
+                    goToLogin = true
+                }
+            } label: {
+                Text("Get Started")
+                    .font(.subheading1())
+                    .bold()
+                    .foregroundColor(.backgroundPrimary)
+                    .padding(.vertical, 20)
+                    .padding(.horizontal, 40)
+                    .background(Capsule()
+                                    .foregroundColor(.accent)
+                                    .shadow(radius: 12, x: 0, y: 3))
+            }
+            Spacer()
+            Spacer()
         }
+        .background(Color.backgroundPrimary)
+        .overlay(
+            Group {
+                if goToLogin {
+                    LoginScreen()
+                        .transition(.move(edge: .trailing))
+                }
+            }
+        )
     }
 }
 
