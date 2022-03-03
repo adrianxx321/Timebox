4d7b7db6213f6d3064900e505015002fc10d5477
adrianxx321 <lianghan321@gmail.com>
2022-03-03 13:38:38 +0800

    Extend View to have more useful modifiers

---
+++ Timebox/View/Extension/View+extension.swift
@@ -0,0 +1,34 @@
+//
+//  View+extension.swift
+//  Timebox
+//
+//  Created by Lianghan Siew on 02/03/2022.
+//
+
+import SwiftUI
+
+extension View {
+    // MARK: Define custom modifier for corner radius with specific edges
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+    
+    // MARK: Allows conditional statement to be applied to any view
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
