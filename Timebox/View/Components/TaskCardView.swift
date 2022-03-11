aebb547cd538ed5b155a77b365570f5bfa9529e1
adrianxx321 <lianghan321@gmail.com>
2022-03-11 18:36:08 +0800

    Simplify conditional views rendering

--- Timebox/View/Components/TaskCardView.swift
+++ Timebox/View/Components/TaskCardView.swift
@@ -13,72 +13,133 @@ struct TaskCardView: View {
     @GestureState private var isDragging = false
     
     var body: some View {
-        HStack(spacing: 16) {
+        ZStack {
+            // Swipe to action buttons...
+            HStack(spacing: 8) {
+                Spacer()
+                
+                // Edit...
+                SwipeToButton(isDestructive: false, action: {
+                    // TODO: Brings up edit modal...
+                })
+                
+                // Delete...
+                SwipeToButton(isDestructive: true, action: {
+                    // TODO: Perform task deletion...
+                })
+            }
             
-            // MARK: Task label color
-            Capsule()
-                .fill(Color(task.color))
-                .frame(width: 6)
-                .frame(minHeight: 32)
-            
-            // MARK: Task information
-            VStack(alignment: .leading ,spacing: 8) {
-                
-                // Show the important label only if it's marked so
-                if task.isImportant {
-                    Text("!!! IMPORTANT")
-                        .font(.caption())
-                        .fontWeight(.semibold)
-                        .foregroundColor(.uiOrange)
-                }
-                
-                Text(task.taskTitle)
-                    .font(.paragraphP1())
-                    .fontWeight(.semibold)
-                    .foregroundColor((task.isCompleted || (taskModel.isScheduledTask(task) && Date() > task.taskEndTime!)) ? .textTertiary : .textPrimary)
-                    .if(task.isCompleted) { text in
-                        text.strikethrough()
-                    }
-                    .multilineTextAlignment(.leading)
-                
-                // Show the subtasks completed info if there's any subtask
-                if task.subtasks.count > 0 {
-                    HStack(spacing: 8) {
-                        Image("branch")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 24)
-                            .rotationEffect(.degrees(90))
-
-                        Text("\(taskModel.countCompletedSubtask(task.subtasks))/\(task.subtasks.count) completed")
+            // Clickable task card which leads to Task Details screen...
+            NavigationLink(destination: TaskDetails(selectedTask: task)) {
+                HStack(spacing: 16) {
+                    // Task label color...
+                    Capsule()
+                        .fill(Color(task.color))
+                        .frame(width: 6)
+                        .frame(minHeight: 32)
+                    
+                    // Task information...
+                    VStack(alignment: .leading ,spacing: 8) {
+                        // Show the important label only if it's marked so...
+                        task.isImportant ?
+                        Text("!!! IMPORTANT")
                             .font(.caption())
                             .fontWeight(.semibold)
-                    }
-                    .foregroundColor((task.isCompleted || (taskModel.isScheduledTask(task) && Date() > task.taskEndTime!)) ? .textTertiary : .textSecondary)
-                }
-                
-                // Show the task duration if
-                //      1. It has time constaint
-                //      2. It is not all-day long
-                if taskModel.isScheduledTask(task) && !taskModel.isAllDayTask(task) {
-                    Text("\(taskModel.formatDate(date: task.taskStartTime!, format: "h:mm a")) - \(taskModel.formatDate(date: task.taskEndTime!, format: "h:mm a"))")
-                        .font(.caption())
-                        .fontWeight(.semibold)
+                            .foregroundColor(.uiOrange)
+                        : nil
+                        
+                        // Task title...
+                        Text(task.taskTitle)
+                            .font(.paragraphP1())
+                            .fontWeight(.semibold)
+                            .foregroundColor((task.isCompleted || (taskModel.isScheduledTask(task) && Date() > task.taskEndTime!)) ? .textTertiary : .textPrimary)
+                            .if(task.isCompleted) { text in
+                                text.strikethrough()
+                            }
+                            .multilineTextAlignment(.leading)
+                        
+                        // Show the subtasks completion info if there's any...
+                        task.subtasks.count > 0 ?
+                        HStack(spacing: 8) {
+                            Image("branch")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 24)
+                                .rotationEffect(.degrees(90))
+
+                            Text("\(taskModel.countCompletedSubtask(task.subtasks))/\(task.subtasks.count) completed")
+                                .font(.caption())
+                                .fontWeight(.semibold)
+                        }
+                        // Text color varies depending on overdue and/or completion status...
                         .foregroundColor((task.isCompleted || (taskModel.isScheduledTask(task) && Date() > task.taskEndTime!)) ? .textTertiary : .textSecondary)
+                        : nil
+                        
+                        // Show the task duration if
+                        // 1. It has time constaint
+                        // 2. It is not all-day long
+                        taskModel.isScheduledTask(task) && !taskModel.isAllDayTask(task) ?
+                        Text("\(taskModel.formatDate(date: task.taskStartTime!, format: "h:mm a")) - \(taskModel.formatDate(date: task.taskEndTime!, format: "h:mm a"))")
+                            .font(.caption())
+                            .fontWeight(.semibold)
+                            // Text color varies depending on overdue and/or completion status...
+                            .foregroundColor((task.isCompleted || (taskModel.isScheduledTask(task) && Date() > task.taskEndTime!)) ? .textTertiary : .textSecondary)
+                        : nil
+                    }
+                    
+                    Spacer()
+                    
+                    // Task checkmark, for task with no subtask...
+                    task.subtasks.count <= 0 ?
+                    Button {
+                        withAnimation {
+                            // Perform update onto Core Data...
+                            task.isCompleted.toggle()
+                        }
+                    } label: {
+                        Image(task.isCompleted ? "checked" : "unchecked")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 28)
+                            .foregroundColor(task.isCompleted ? .accent : .backgroundTertiary)
+                            .padding(.trailing, 8)
+                    }
+                    : nil
                 }
+                .padding(16)
+                .background(Color.uiWhite)
+                .cornerRadius(16)
+                // Swipe gesture...
+                .offset(x: CGFloat(task.offset))
+                .gesture(DragGesture()
+                            .updating($isDragging, body: { (value, state, _) in
+                                // so we can validate for correct drag
+                                state = true
+                                onChanged(value: value)
+                            }).onEnded({ (value) in
+                                onEnd(value: value)
+                            }))
             }
-            
-            Spacer()
-            
-            // MARK: Task checkmark
-            if task.subtasks.count <= 0 {
-                Image("check-circle-f")
-                    .padding(.trailing, 8)
-            }
+            .buttonStyle(FlatLinkStyle())
         }
-        .frame(width: UIScreen.main.bounds.width - 80, alignment: .leading)
-        .padding(16)
-        .background(Color.uiWhite)
+        .frame(width: UIScreen.main.bounds.width - 48, alignment: .leading)
+    }
+    
+    func SwipeToButton(isDestructive: Bool, action: @escaping () -> Void) -> some View {
+        Button {
+            withAnimation {
+                action()
+            }
+        } label: {
+            Image(isDestructive ? "trash" : "pencil")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 24)
+                .foregroundColor(isDestructive ? .uiRed : .textSecondary)
+        }
+        .padding(.horizontal, 16)
+        .frame(maxHeight: .infinity)
+        .background(Color(isDestructive ? .uiPink : .uiWhite))
         .cornerRadius(16)
     }
     
